2fb8a5515bb6034d517ae96bb694b9e299588854
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2025-01-05 19:01:59 +0530

    Update Qn_1_Arularasan.java

--- Qn_1_Arularasan.java
+++ Qn_1_Arularasan.java
@@ -1,1 +1,11 @@
 import java.util.*;
+public class Main{
+public static void main(String[] args){
+  int a=3;
+  System.out.print(factorial(a));//3*2*1 = 6
+}
+  public static int factorial(int a){
+    if(a==0) return 1;
+    return a*factorial(a-1);
+  }
+}
